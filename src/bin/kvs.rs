227e08e9381dce91063fa243920d5764c50c212d
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-02-15 22:22:27 +0700

    Added base cli, that accepts subcommand and its corresponding argument

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,6 +1,30 @@
+extern crate clap;
+use clap::{Parser, Subcommand};
 
-fn main() {
-    todo!();
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+
+    #[command(subcommand)]
+    command: Option<Commands>,
 }
 
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Set{key: String, val: String},
+    Get{key: String},
+    Remove{key:String}
 
+}
+
+fn main() {
+    let cli = Cli::parse();
+    // Your implementation here
+    match &cli.command.unwrap() {
+        Commands::Get { key } => println!("{}",key),
+        Commands::Remove { key } => todo!(),
+        Commands::Set { key, val } => todo!()
+        
+    }
+}
