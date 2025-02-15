30b23fa036aeedd1772f8eb6fcd68056a67abd08
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-02-15 17:32:49 +0700

    base

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+#[derive(Debug)]
+pub struct KvStore {
+
+}
+
+impl KvStore {
+    pub fn new() -> KvStore{
+        todo!()
+    }
+
+    pub fn set(&self, key: String, val: String){
+        todo!()
+    }
+
+    pub fn get(&self, key: String){
+        todo!()
+    }
+
+    pub fn remove(&self, ){
+        todo!();
+    }
+    
+}
+
