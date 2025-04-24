ca0cb8eff6932dd26c77ba0c3545961a7b9ac14e
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-04-24 21:05:03 +0700

    Basic Cli for kvs-client

--- src/bin/kvs-client.rs
+++ src/bin/kvs-client.rs
@@ -1,1 +1,65 @@
+use std::{env::current_dir,  process::exit, str::FromStr};
+use clap::{Parser, Subcommand};
+use ferris::kvstore::KvStore;
 
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    #[arg(short,long)]
+    address: String,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[allow(non_camel_case_types)]
+    /// Set a key-value pair
+    set { key: String, val: String },
+
+    /// Get the value for a key
+    #[allow(non_camel_case_types)]
+    get { key: String },
+    
+    /// Remove a key-value pair
+    #[allow(non_camel_case_types)]
+    rm { key: String },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    if cli.command.is_none() {
+        Cli::parse_from(["kvs", "--help"]);
+        return;
+    }
+
+    // Your implementation here
+    match &cli.command.unwrap() {
+        Commands::get { key } => {
+            let val = Ok(key);
+            match val.unwrap() {
+                Some(d) => println!("{}", d),
+                None => println!("Key not found"),
+            }
+        }
+
+        Commands::rm { key } => {
+            let res = Ok(key);
+            match res {
+                Ok(_) => (),
+                Err(_) => {
+                    println!("Key not found");
+                    exit(1);
+                }
+            }
+            println!("Key removed succesfully");
+        }
+
+        Commands::set { key, val } => {
+            let _ = Ok((key,val));
+            println!("Key set succesfully");
+        }
+    }
+}
